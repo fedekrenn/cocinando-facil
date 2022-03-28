b843ed0389a0cd917fa51abab871deb5b3c72bed
Federico Krenn <fedekrenn@gmail.com>
2022-03-28 12:23:53 -0300

    Se añaden viejas funciones

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ class Recetas {
         this.ingredientes = ingredientes;
         this.tiempoDeCoccion = tiempoDeCoccion;
     }
+    longitud() {                                      // Método para conocer cantidad de ingredientes
+        return (this.ingredientes).length;
+    }
+    recetaConCarne() {                                // Método para saber si la receta tiene carne
+        return (this.ingredientes).some((val) => val == "carne" || val == "Carne");
+    }
 }
 
 
@@ -157,3 +163,78 @@ function imprimirReceta(array) {
     //Reseteo el array, ya que si no cuando se da a guardar se crean muchas veces tablas con recetas repetidas
     listadoDeRecetas = [];
 }
+
+
+
+
+
+// NUEVOOOOOO
+
+/*
+
+//Función que compara la cantidad de ingredientes para determinar cuál receta es más complicada de cocinar, se usa el método .longitud() del objeto 
+function masDificilDeCocinar(primerReceta, segundaReceta) {
+    if ((primerReceta.longitud()) > (segundaReceta.longitud())) {
+        alert(`Cocinar ${primerReceta.nombre} es más difícil ya que lleva ${primerReceta.longitud()} ingredientes, en cambio ${segundaReceta.nombre} lleva sólamente ${segundaReceta.longitud()}`);
+    } else if ((primerReceta.longitud()) == (segundaReceta.longitud())){
+        alert(`Ambas recetas tienen la misma dificultad ya que tienen la misma cantidad de ingredientes (${primerReceta.longitud()})`);
+    } else {
+        alert(`Cocinar ${segundaReceta.nombre} es más difícil ya que lleva ${segundaReceta.longitud()} ingredientes, en cambio ${primerReceta.nombre} lleva sólamente ${primerReceta.longitud()}`);
+    }
+}
+
+
+
+// Creación de arrays de comidas vegetarianas, comidas rápidas y tiempo total para cocinar todas las recetas
+const comidasVegetarianas = (listadoRecetas.filter((val) => val.recetaConCarne() == false)).map((el) => el.nombre);
+const comidasRapidas = (listadoRecetas.filter((val) => val.tiempoDeCoccion <= 20)).map((el) => el.nombre);
+const tiempoTotalDeCocina = (listadoRecetas.map((val) => val.tiempoDeCoccion)).reduce((acc, el) => acc + el, 0);
+
+
+
+alert(`El tiempo total para cocinar todas esas recetas es de ${tiempoTotalDeCocina} minutos`);
+
+
+
+// Condicional para mostrar por alerta comidas vegetarianas y rápidas solo si el array contiene elementos
+if (comidasVegetarianas.length != 0) {
+    alert(`Las comidas aptas para vegetarianos son:\n${comidasVegetarianas.join(`\n`)}`);
+}
+
+if (comidasRapidas.length != 0) {
+    alert(`Estas son las comidas más rápidas de cocinar:\n${comidasRapidas.join(`\n`)}`);
+}
+
+
+
+// El código a continuación crea un listado de los nombres de las recetas y adicionalmente le agrega un contador 1 -, 2-, etc según cuantas recetas hay
+const contador = listadoRecetas.map((val) => val.nombre);
+const contadorConRecetas = [];
+
+for (let i = 0; i < contador.length; i++) {
+    contadorConRecetas[i] = `${i + 1} - ${contador[i]}`;
+}
+
+*/
+
+
+
+/*Lanzador de la función masDificilDeCocinar(), está dentro de un if, para que el usuario pueda elegir
+que recetas comparar en el caso de que haya 3 o más, en el caso de ser 2 recetas las compara automáticamente
+y en el caso que haya 1 o ninguna no efectua comparación
+*/
+
+/*
+if (listadoRecetas.length > 2) {
+    alert(`Usted ha seleccionado ${cantidadDeseada} recetas, vamos a comparar 2 de ellas para ver cual es más difícil de cocinar según sus ingredientes`);
+    let comparacionReceta1 = listadoRecetas[(parseInt(prompt(`Ingrese el número de receta del 1 al ${cantidadDeseada}\n${contadorConRecetas.join("\n")}`)) - 1)];
+    let comparacionReceta2 = listadoRecetas[(parseInt(prompt(`Ahora ingresá otro número diferente al anterior, del 1 al ${cantidadDeseada}\n${contadorConRecetas.join("\n")}`)) - 1)];
+    masDificilDeCocinar(comparacionReceta1, comparacionReceta2);
+} else if (listadoRecetas.length == 2) {
+    alert(`Usted ha seleccionado ${cantidadDeseada} recetas, a continuación compararemos cuál es más difícil de cocinar en base a sus ingredientes`);
+    masDificilDeCocinar(listadoRecetas[0], listadoRecetas[1]);
+} else {
+    alert("No se ingresaron las suficientes recetas para realizar una comparación");
+}
+
+*/
