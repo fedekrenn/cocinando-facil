3a0501e1ce2b469fe8a81a14f73324add287f747
Federico Krenn <fedekrenn@gmail.com>
2022-04-12 23:55:56 -0300

    Se soluciona bug en logueo por ciclo for

--- js/loguin.js
+++ js/loguin.js
@@ -1,49 +1,78 @@
+// Tomo los elementos de los formularios de logueo y creación de cuenta
 let formularioAcceso = document.getElementById("formularioAcceso");
 let formularioRegistro = document.getElementById("formularioRegistro");
 
 
-if (formularioAcceso != null) {
-    formularioAcceso.addEventListener("submit", loguearse);
-}
-if (formularioRegistro != null){
-    formularioRegistro.addEventListener("submit", crearCuenta);
-}
+// Creación de array con todos los usuarios que se van creando
+let usuariosRegistrados = [];
+
+
+//Se usa el operador AND para comprobar que no haya error por null al traer los form (ya que son 2 html distintos)
+formularioAcceso != null && formularioAcceso.addEventListener("submit", loguearse);
+formularioRegistro != null && formularioRegistro.addEventListener("submit", crearCuenta);
 
 
 
-function crearCuenta(e){
-    event.preventDefault();
 
 
-    let nombreDeUsuario = document.getElementById("username").value;
-    let email = document.getElementById("email").value;
-    let pass = document.getElementById("password").value;
+
+function crearCuenta(e) {
+    e.preventDefault();
+
+    let nombreDeUsuario = document.getElementById("username");
+    let email = document.getElementById("email");
+    let pass = document.getElementById("password");
 
     let user = {
-        email: email,
-        nombreDeUsuario: nombreDeUsuario,
-        pass: pass
+        email: email.value,
+        nombreDeUsuario: nombreDeUsuario.value,
+        pass: pass.value
     }
+    usuariosRegistrados.push(user);
+  
+    let json = JSON.stringify(usuariosRegistrados);
+    localStorage.setItem("username", json);
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'El usuario se cargó correctamente',
+        showConfirmButton: false,
+        timer: 1500
+    });
 
-    let json = JSON.stringify(user);
-    localStorage.setItem(username, json);
-    alert("Usuario agregado");
-    window.location = '../index.html'
+    nombreDeUsuario.value = "";
+    email.value = "";
+    pass.value ="";
 }
 
-function loguearse(e){
-    event.preventDefault();
+function loguearse(e) {
+    e.preventDefault();
 
     let nombreDeUsuario = document.getElementById("username").value;
     let pass = document.getElementById("password").value;
 
-    let user = localStorage.getItem(username);
+    let user = localStorage.getItem("username");
     let datos = JSON.parse(user);
 
-    
-    if (nombreDeUsuario == datos.nombreDeUsuario && pass == datos.pass){
+    if (user == null) {
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Usuario / Contraseña incorrecta',
+            confirmButtonText: 'Reintentar',
+            timer: 1500
+        });
+    } else if (datos.some((val) => val.nombreDeUsuario == nombreDeUsuario) && datos.some((val) => val.pass == pass)) {
         window.location = 'pages/sesion.html'
     } else {
-        alert("Mmmm, no maestro");
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Usuario / Contraseña incorrecta',
+            confirmButtonText: 'Reintentar',
+            timer: 1500
+        });
     }
+
+
 }
